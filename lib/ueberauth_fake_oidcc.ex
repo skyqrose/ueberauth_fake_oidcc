e64a9b1d3ee463d57146e1743f53bb1560ef5442
Sky Rose <srose@mbta.com>
2026-01-20 10:18:58 -0500

    mix new

---
+++ lib/ueberauth_fake_oidcc.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthFakeOidcc do
+  @moduledoc """
+  Documentation for `UeberauthFakeOidcc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthFakeOidcc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_fake_oidcc_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthFakeOidccTest do
+  use ExUnit.Case
+  doctest UeberauthFakeOidcc
+
+  test "greets the world" do
+    assert UeberauthFakeOidcc.hello() == :world
+  end
+end

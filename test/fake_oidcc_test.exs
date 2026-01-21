69e89fe7e2fa924360cf4db18b50e4c2794b4984
Sky Rose <srose@mbta.com>
2026-01-21 18:17:21 -0500

    more test

--- test/fake_oidcc_test.exs
+++ test/fake_oidcc_test.exs
@@ -56,10 +56,59 @@ defmodule Ueberauth.Strategy.FakeOidccTest do
                  refresh_token: "fake_refresh_token"
                },
                extra: %Ueberauth.Auth.Extra{
-                 # "roles" => [],
-                 # "resource_access" => %{
-                 #   "fake_client_id" => %{"roles" => []}
-                 # },
+                 raw_info: %UeberauthOidcc.RawInfo{
+                   claims: %{
+                     "auth_time" => _
+                   },
+                   userinfo: %{
+                     "roles" => [],
+                     "resource_access" => %{
+                       "fake_client_id" => %{"roles" => []}
+                     }
+                   }
+                 }
+               }
+             } = conn.assigns.ueberauth_auth
+    end
+
+    test "works with full config" do
+      conn =
+        conn(:get, "/auth/providername/callback?email=test@test.example&roles[]=role1&roles[]=role2")
+        |> init_test_session(%{})
+        |> Plug.Conn.fetch_query_params()
+        |> Ueberauth.run_callback(:providername, {FakeOidcc, [
+          client_id: "custom_client_id",
+          credentials: %{other: %{custom_field: "value1"}},
+          uid: "custom_uid",
+          userinfo: %{"custom_field" => "value2"}
+        ]})
+
+      assert Map.get(conn.assigns, :ueberauth_failure) == nil
+
+      assert %Ueberauth.Auth{
+               uid: "custom_uid",
+               provider: :providername,
+               info: %Ueberauth.Auth.Info{
+                 email: "test@test.example"
+               },
+               credentials: %Ueberauth.Auth.Credentials{
+                 token: "fake_access_token",
+                 refresh_token: "fake_refresh_token",
+                 other: %{custom_field: "value1"}
+               },
+               extra: %Ueberauth.Auth.Extra{
+                 raw_info: %UeberauthOidcc.RawInfo{
+                   claims: %{
+                     "auth_time" => _
+                   },
+                   userinfo: %{
+                     "custom_field" => "value2",
+                     "roles" => ["role1", "role2"],
+                     "resource_access" => %{
+                       "custom_client_id" => %{"roles" => ["role1", "role2"]}
+                     }
+                   }
+                 }
                }
              } = conn.assigns.ueberauth_auth
     end

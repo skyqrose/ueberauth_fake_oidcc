924d93303f5786c5b8594f691acd536bf589e2da
Sky Rose <srose@mbta.com>
2026-01-21 14:13:41 -0500

    more test

--- test/fake_oidcc_test.exs
+++ test/fake_oidcc_test.exs
@@ -4,6 +4,7 @@ defmodule Ueberauth.Strategy.FakeOidccTest do
 
   import Plug.Test
 
+  alias Phoenix.ConnTest
   alias Ueberauth.Strategy.FakeOidcc
 
   describe "handle_request!" do
@@ -23,6 +24,15 @@ defmodule Ueberauth.Strategy.FakeOidccTest do
       conn =
         conn(:get, "/auth/providername")
         |> init_test_session(%{})
+        |> Ueberauth.run_request(:providername, {FakeOidcc, FakeOidcc.default_options()})
+
+      assert conn.resp_body =~ "Log in"
+    end
+
+    test "renders login page with empty config" do
+      conn =
+        conn(:get, "/auth/providername")
+        |> init_test_session(%{})
         |> Ueberauth.run_request(:providername, {FakeOidcc, []})
 
       assert conn.resp_body =~ "Log in"
@@ -30,6 +40,7 @@ defmodule Ueberauth.Strategy.FakeOidccTest do
   end
 
   describe "handle_callback" do
+    # also covers uid, credentials, info, extra
     test "works" do
       conn =
         conn(:get, "/auth/providername/callback?email=test@test.example")
@@ -38,6 +49,33 @@ defmodule Ueberauth.Strategy.FakeOidccTest do
         |> Ueberauth.run_callback(:providername, {FakeOidcc, []})
 
       assert Map.get(conn.assigns, :ueberauth_failure) == nil
+      assert %Ueberauth.Auth{
+        uid: "fake_uid",
+        provider: :providername,
+        info: %Ueberauth.Auth.Info{
+          email: "test@test.example"
+        },
+        credentials: %Ueberauth.Auth.Credentials{
+          token: "fake_access_token",
+          refresh_token: "fake_refresh_token",
+          # TODO assert expires here?
+        },
+        extra: %Ueberauth.Auth.Extra{
+          # TODO groups?
+          # under client_id
+        }
+      } = conn.assigns.ueberauth_auth
+
+    end
+
+    test "is invalid without email" do
+      conn =
+        conn(:get, "/auth/providername/callback")
+        |> init_test_session(%{})
+        |> Plug.Conn.fetch_query_params()
+        |> Ueberauth.run_callback(:providername, {FakeOidcc, []})
+
+      assert Map.get(conn.assigns, :ueberauth_failure) != nil
     end
 
     test "is invalid if given invalid param" do
@@ -48,6 +86,15 @@ defmodule Ueberauth.Strategy.FakeOidccTest do
         |> Ueberauth.run_callback(:providername, {FakeOidcc, []})
 
       assert Map.get(conn.assigns, :ueberauth_failure) != nil
+    end
+  end
+
+  describe "handle_cleanup!" do
+    test "doesn't crash" do
+      # doesn't do anything, but Ueberauth calls it and it's not covered above
+      # so verify it doesn't crash I guess
+      conn = Phoenix.ConnTest.build_conn()
+      assert FakeOidcc.handle_cleanup!(conn) == conn
     end
   end
 end
